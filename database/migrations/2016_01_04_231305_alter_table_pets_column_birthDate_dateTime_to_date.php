0c0216a485c400ecc52a08a518f7c6a1e5862d3c
franciscoBarrientos <francisco.barrientos.a@gmail.com>
2016-01-04 23:21:57 -0300

    Modificacion de la columna de año de nacimiento de mascota

---
+++ database/migrations/2016_01_04_231305_alter_table_pets_column_birthDate_dateTime_to_date.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTablePetsColumnBirthDateDateTimeToDate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('pets', function ($table) {
+            $table->dropColumn('birth_date');
+        });
+
+        Schema::table('pets', function ($table) {
+            $table->date('birth_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('pets', function ($table) {
+            $table->dropColumn('birth_date');
+        });
+
+        Schema::table('pets', function ($table) {
+            $table->datetime('birth_date');
+        });
+    }
+}
